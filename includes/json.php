862555e7f8852b0620f36a4c26041eaa6fcd1783
Alexis Paques <alexis.paques@gmail.com>
2015-03-26 18:32:14 +0100

    Update json.php to use only one file

--- includes/json.php
+++ includes/json.php
@@ -74,4 +74,11 @@
 			$this->json = "\"Text\": \"{$value}\",";
 		}
 	}
+	
+	function json_send($json){
+		header('Cache-Control: no-cache, must-revalidate');
+		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+		header('Content-type: application/json');//*/
+		echo $json->json_make();
+	}
 ?>
