c8bd91f090718816542a14e22be9af75de7733df
Alexis Paques <alexis.paques@gmail.com>
2015-03-26 18:32:50 +0100

    Update json.php to use only one file

--- includes/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-	function json_send($json){
-		header('Cache-Control: no-cache, must-revalidate');
-		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-		header('Content-type: application/json');//*/
-		echo $json->json_make();
-	}
-?>
