9084cf7ef7f74be16167b17ba46a0d09ad495261
Alexis Paques <alexis.paques@gmail.com>
2015-03-27 15:02:45 +0100

    travis

--- test/Phpunit.php
+++ test/Phpunit.php
@@ -36,5 +36,44 @@ class Phpunit extends PHPUnit_Framework_TestCase
         $Json->addContent(new arrayJson("An_Array",$arraytest));
         $Json->addContent(new jsonJson("A_Json",$jsonOnly));
     }
+    public function testPropertyJson()
+    {
+        $Json = new json();
+        $Json->addContent(new propertyJson('width', '565px'));
+        $this->assertEquals($Json->json_make(),
+            '{"width" : "565px"}');
+    }
+    public function testTextJson()
+    {
+        $Json = new json();
+        $Json->addContent(new textJson('You are logged IN'));
+        $this->assertEquals($Json->json_make(),
+            '{"Text": "You are logged IN"}');
+    }
+    public function testArrayJson()
+    {
+        $Json = new json();
+        $arraytest = array('1','2','3');
+        $Json->addContent(new arrayJson("An_Array",$arraytest));
+        $this->assertEquals($Json->json_make(),
+            '{"An_Array": ["1","2","3"]}');
+    }
+    public function testJsonJson()
+    {
+        $Json = new json();
+        $jsonOnly = '{"Hello" : "darling"}';
+        $Json->addContent(new jsonJson("A_Json",$jsonOnly));
+        $this->assertEquals($Json->json_make(),
+            '{"A_Json": {"Hello" : "darling"}}');
+    }
+    public function testObjectJson()
+    {
+        $Json = new json();
+        $object = new stdClass();
+        $object->test = 'OK';
+        $Json->addContent(new objectJson('An_Object', $object));
+        $this->assertEquals($Json->json_make(),
+            '{"An_Object": {"test":"OK"}}');
+    }
 }
 ?>
